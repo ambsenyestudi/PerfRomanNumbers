61ae5963ca3452a558c56f1e32d9f229775ea771
ambsenyestudi <adrifre7@hotmail.com>
2021-04-30 06:56:51 +0200

    No unit part refactor

--- src/RomanNumbers/RomanNumbers.RDM.Domain/RomanNumber.cs
+++ src/RomanNumbers/RomanNumbers.RDM.Domain/RomanNumber.cs
@@ -4,6 +4,8 @@ namespace RomanNumbers.RDM.Domain
 {
     public class RomanNumber
     {
+        private const int EMPTY_UNIT = 0;
+        private const int MAX_ALLOWED_UNITS = 3;
         private readonly string[] UNIT_LIST = new string[] { "I", "II", "III" };
         private const int HALF_TEN_ARABIC = 5;
         private const string HALF_TEN = "V";
@@ -15,18 +17,29 @@ namespace RomanNumbers.RDM.Domain
         private string FigureNumbers(int arabic)
         {
             var result = "";
+
             if(arabic == 4)
             {
                 return "IV";
             }
+
             if(arabic >= HALF_TEN_ARABIC)
             {
                 result = HALF_TEN;
                 arabic -= HALF_TEN_ARABIC;
             }
-            var unitIndex = arabic - 1;
-            return result + UNIT_LIST[unitIndex];
+
+            if(HasUnitPart(arabic))
+            {
+                var unitIndex = arabic - 1;
+                result += UNIT_LIST[unitIndex];
+            }
+
+            return result;
         }
+        private bool HasUnitPart(int arabic) =>
+            arabic > EMPTY_UNIT &&
+            arabic <= MAX_ALLOWED_UNITS;
 
 
         public override bool Equals(object obj)
