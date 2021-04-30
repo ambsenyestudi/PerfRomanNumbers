e95adef12b0eebf0ebbf6898e8f2ff4021550317
ambsenyestudi <adrifre7@hotmail.com>
2021-04-30 06:30:10 +0200

    parse 3 test red

--- src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
+++ src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
@@ -9,6 +9,7 @@ namespace RomanNumbers.RDM.Test
         [Theory]
         [InlineData(1,"I")]
         [InlineData(2, "II")]
+        [InlineData(3, "III")]
         public void ParseOne(int input, string exptected)
         {
             var sut = new RomanNumber(input);
