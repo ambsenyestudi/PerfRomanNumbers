7a47291ecd316fd3dcda288d985fa9fa22718e6d
ambsenyestudi <adrifre7@hotmail.com>
2021-04-30 06:44:49 +0200

    Parse 4 red fase

--- src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
+++ src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
@@ -10,6 +10,7 @@ namespace RomanNumbers.RDM.Test
         [InlineData(1,"I")]
         [InlineData(2, "II")]
         [InlineData(3, "III")]
+        [InlineData(4, "IV")]
         [InlineData(5, "V")]
         [InlineData(6, "VI")]
         public void ParseOne(int input, string exptected)
