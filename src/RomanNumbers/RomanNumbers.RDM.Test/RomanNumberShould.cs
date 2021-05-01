f9831b65867efd1f58ec9a36857137fc2318d316
ambsenyestudi <adrifre7@hotmail.com>
2021-05-01 08:45:31 +0200

    Test refactor

--- src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
+++ src/RomanNumbers/RomanNumbers.RDM.Test/RomanNumberShould.cs
@@ -9,19 +9,29 @@ namespace RomanNumbers.RDM.Test
         [InlineData(1,"I")]
         [InlineData(2, "II")]
         [InlineData(3, "III")]
-        [InlineData(4, "IV")]
         [InlineData(5, "V")]
         [InlineData(6, "VI")]
-        [InlineData(9, "IX")]
         [InlineData(10, "X")]
         [InlineData(13, "XIII")]
         [InlineData(15, "XV")]
-        [InlineData(19, "XIX")]
-        public void ParseOne(int input, string exptected)
+        [InlineData(17, "XVII")]
+        public void ParseArabic(int input, string exptected)
         {
             var sut = new RomanNumber(input);
             Assert.True(sut.Equals(exptected));
         }
+
+        [Theory]
+        [InlineData(4, "IV")]
+        [InlineData(9, "IX")]
+        [InlineData(14, "XIV")]
+        [InlineData(19, "XIX")]
+        public void ParseUnitBefore(int input, string exptected)
+        {
+            var sut = new RomanNumber(input);
+            Assert.True(sut.Equals(exptected));
+        }
+
 
         [Theory]
         [InlineData(4, "V")]
