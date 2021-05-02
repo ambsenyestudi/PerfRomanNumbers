e92febe32736cd1fdddf71a2696d8e3df7787cf0
ambsenyestudi <adrifre7@hotmail.com>
2021-05-02 20:51:56 +0200

    Arabic calculation refacotr

--- src/RomanNumbers/RomanNumbers.RDM.Domain/RomanConvertible.cs
+++ src/RomanNumbers/RomanNumbers.RDM.Domain/RomanConvertible.cs
@@ -9,40 +9,49 @@ namespace RomanNumbers.RDM.Domain
         public string RomanValue { get; }
         protected RomanConvertible(string romanValue, int arabicValue) => (ArabicValue, RomanValue) = (arabicValue, romanValue);
 
-        protected RomanConvertible(RomanSymbol[] romanSymbolList)
+        protected RomanConvertible(RomanSymbol[] romanSymbolList): this(string.Join("", romanSymbolList.ToList()), 
+            ToArabicValue(romanSymbolList))
         {
-            ArabicValue = ToArabicValue(romanSymbolList);
-            RomanValue = string.Join("", romanSymbolList.ToList());
         }
         
-
         public static int ToArabicValue(IEnumerable<RomanSymbol> romanSymbolCollection)
         {
             if(romanSymbolCollection == null || !romanSymbolCollection.Any())
             {
                 return 0;
             }
+
             var result = 0;
             var romanSymbolList = romanSymbolCollection.ToArray();
             for (int i = 0; i < romanSymbolList.Length; i++)
             {
+                
                 var currentSymbol = romanSymbolList[i];
                 var currResult = currentSymbol.ArabicValue;
                 
-                if (CanPullNext(i, romanSymbolList.Length))
+                if(TryGetNextSymbol(i, romanSymbolList, out RomanSymbol nextSymbol) &&
+                    TrySubstractNextFromCurrentSymbol(nextSymbol, currentSymbol, out int partialResult))
                 {
-                    var nextIndex = i + 1;
-                    var next = romanSymbolList[nextIndex];
-                    if(next.ArabicValue > currentSymbol.ArabicValue)
-                    {
-                        currResult = next.ArabicValue - currResult;
-                        i++;
-                    }
+                    currResult = partialResult;
+                    i++;
                 }
                 
                 result += currResult;
 
             }
+            return result;
+        }
+        private static bool TrySubstractNextFromCurrentSymbol(RomanSymbol next, RomanSymbol current, out int result)
+        {
+            result = next.ArabicValue - current.ArabicValue;
+            return result > 0;
+        }
+        private static bool TryGetNextSymbol(int index, RomanSymbol[] list, out RomanSymbol next)
+        {
+            var result = CanPullNext(index, list.Length);
+            next = result 
+                ? list[index + 1]
+                : null;
             return result;
         }
         private static bool CanPullNext(int index, int range) =>
